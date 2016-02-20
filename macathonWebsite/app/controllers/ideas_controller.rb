5d584e48e141080c3dac192fe7ea4413bf2d06c5
Paul Chery <paulachery@gmail.com>
2016-02-19 21:14:44 -0600

    Added user input, link from welcome page

--- macathonWebsite/app/controllers/ideas_controller.rb
+++ macathonWebsite/app/controllers/ideas_controller.rb
@@ -1,15 +1,26 @@
 class IdeasController < ApplicationController
  
   def new
+    @idea = Idea.new
   end
 
   def create
+    @idea = Idea.new(idea_params)
+    if @idea.save	
+      redirect_to new_idea_path
+    end 
   end
 
   def destroy
   end 
 
+  def randShow
+  end 
 
+  private
+   def idea_params
+   params.require(:idea).permit(:name)
+  end
 
 
 end
--- macathonWebsite/config/routes.rb
+++ macathonWebsite/config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'welcome#index'
+  resources :ideas
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
